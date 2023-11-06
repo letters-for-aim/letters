3ddc613b0ca635f905e2cb3d153328ff2612ca3b
Sataphon Obra <85420217+puyepuye@users.noreply.github.com>
2023-11-06 15:36:06 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+
+let isCardFront = true; // Indicates if the card is in the front position
+const card = document.getElementById("card");
+
+
+card.addEventListener("click", () => {
+    card.classList.toggle("is-rotated");
+    isCardFront = False;
+});
+
+const mailbox = document.getElementById("mailbox");
+const itemsContainer = document.getElementById("items");
+
+
+mailbox.addEventListener("click", () => {
+    itemsContainer.classList.toggle("items-open");
+});
+
+itemsContainer.addEventListener("transitionend", () => {
+    // Check if the items have finished sliding in
+    if (itemsContainer.classList.contains("items-open")) {
+        isCardFront = false;
+    }
+});
+
+const items = document.querySelectorAll(".item");
+items.forEach(item => {
+    item.addEventListener("click", () => {
+        const frontSRC = item.getAttribute("front-image");
+        const backSRC = item.getAttribute("back-image");
+        document.querySelector(".card-front img").src = "cards/front/" + frontSRC;
+        document.querySelector(".card-back img").src = "cards/back/" + backSRC;
+
+
+        itemsContainer.classList.toggle("items-open");
+    });
+});
+
+const itemData = {
+    "nineteenth": {
+        frontImage: "IMG_3558.jpg",
+        backImage: "notes.jpeg"
+    },
+    "japon": {
+        frontImage: "aimjapan.jpg",
+        backImage: "courtesy of osaka.png"
+    },
+   
+   
+
+    // Add more items as needed
+};
+
+for (const itemName in itemData) {
+    const item = document.createElement("div");
+    item.classList.add("item");
+    item.textContent = itemName;
+    item.setAttribute("front-image", itemData[itemName].frontImage);
+    item.setAttribute("back-image", itemData[itemName].backImage);
+    item.addEventListener("click", () => {
+        const frontSRC = item.getAttribute("front-image");
+        const backSRC = item.getAttribute("back-image");
+        document.querySelector(".card-front img").src = "cards/front/" + frontSRC;
+        document.querySelector(".card-back img").src = "cards/back/" + backSRC;
+        if (isCardFront == False) {
+            card.classList.toggle("is-rotated");
+        }
+        itemsContainer.classList.toggle("items-open");
+    });
+    itemsContainer.appendChild(item);
+}
